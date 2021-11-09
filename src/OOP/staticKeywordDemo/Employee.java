7e5609f3434e4019eddb301fda192f72316d0653
Sabin Gt <sabingt0@gmail.com>
2021-11-09 10:42:05 +0545

    Example of static keywords

---
+++ src/OOP/staticKeywordDemo/Employee.java
@@ -0,0 +1,7 @@
+package OOP.staticKeywordDemo;
+
+public class Employee extends User {
+    public static void doAnyStaticAction() {
+        System.out.println("Static method in Employee class is called.");
+    }
+}
